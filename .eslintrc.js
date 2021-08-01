bce2fffc326025c4429ee6ce9e8d434a92405e14
NIHILncunia <nihil_ncunia@naver.com>
2021-08-02 01:05:49 +0900

    2021 0802 템플릿 최신화

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,73 +3,33 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es6: true,
     node: true,
+    es6: true,
   },
   extends: [
+    'airbnb',
+    'prettier',
     'eslint:recommended',
-    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
-  plugins: [
-    'import',
-    'react-hooks',
-    'react',
-  ],
-  settings: {
-    'react': {
-      'version': 'detect',
-    },
-  },
+  plugins: ['import', 'react', 'react-hooks'],
   rules: {
     'no-console': 0,
     'no-unused-vars': 1,
     'no-unexpected-multiline': 0,
-    'no-empty': 1,
-    'semi': [ 2, 'always', ],
-    'quotes': [ 1, 'single', { allowTemplateLiterals: true, }, ],
-    'eol-last': [ 1, 'never', ],
-    'space-before-function-paren': 0,
-    'array-bracket-spacing': [ 1, 'always', {
-      arraysInArrays: true,
-      singleValue: true,
-      objectsInArrays: true,
-    }, ],
-    'arrow-parens': 0,
-    'comma-spacing': [ 1, { before: false, after: true, }, ],
-    'comma-dangle': [ 1, {
-      arrays: 'always',
-      functions: 'never',
-      objects: 'always',
-      imports: 'never',
-      exports: 'never',
-    }, ],
-    'block-spacing': [ 1, 'always', ],
-    'func-call-spacing': [ 1, 'never', ],
-    'object-curly-spacing': [ 1, 'always', ],
-    'arrow-spacing': [ 1, { before: true, after: true, }, ],
-    'keyword-spacing': [ 1, {
-      overrides: {
-        if: { before: true, after: true, },
-        for: { before: true, after: true, },
-        while: { before: true, after: true, },
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', '.jsx'],
       },
-    }, ],
-
-    'jsx-quotes': [ 1, 'prefer-single', ],
-    
-    'react/prop-types': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/display-name': 1,
+    ],
   },
 };
